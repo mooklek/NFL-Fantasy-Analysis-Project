daaa6d11f48d72eac413ec2c7ca9f65bbe140a5a
mooklek <michaelykim03@gmail.com>
2025-01-06 16:21:44 -0800

    Finished Passing.py
    Gathered passing stats and organized it into a csv file.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from receiving import receivingStats
+from passing import passingStats
 
 def main():
+    
     receiver = receivingStats()
     stats = receiver.getStats()
     receiver.write_csv('recStats',['Name', 'Team', 'Age', 'Position',
                                    'Yards', 'Receptions', 'Targets', 
                                    'YardsPerReceptions', 'CatchPercentage', 'Touchdowns'], stats)
+    
+
+    passer = passingStats()
+    stats = passer.getStats()
+    passer.write_csv('pasStats', ['Name', 'Team', 'Age', 'Yards', 
+                                  'Completions', 'Attempts', 'YardsPerPass',
+                                  'Interceptions', 'Touchdowns'], stats)
 
 if __name__ == "__main__":
     main()
--- passing.py
+++ passing.py
@@ -3,28 +3,32 @@ from base_scraper import statsScraper
 
 class passingStats(statsScraper):
     def getStats(self):
+
+        '''inputs passing parameter into fetch_page() funciton in order to get values'''
         self.fetch_page('passing')
-        
-        playerNames = []
-        playerAge = []
-        playerTeams = []
-        playerComp = []
-        playerAtt = []
-        playerPassYards = []
-        playerCompRate = []
-        playerTD = []
-        playerInt = []
         table_tag = 'td'
 
+        '''scrapes website using the html tags given on the website'''
         playerNames = self.find_stat(table_tag, 'name_display')
         playerAge = self.find_stat(table_tag, 'age')
         playerTeams = self.find_stat(table_tag, 'team_name_abbr')
         playerComp = self.find_stat(table_tag, 'pass_cmp')
         playerAtt = self.find_stat(table_tag, 'pass_att')
         playerPassYards = self.find_stat(table_tag, 'pass_yds')
-        playerCompRate = self.find_stat(table_tag, 'pass_cmp_pct')
-        playerTD = self.find_stat(table_tag, 'td')
+        playerYardsPerPass = self.find_stat(table_tag, 'pass_yds_per_att')
+        playerTD = self.find_stat(table_tag, 'pass_td')
         playerInt = self.find_stat(table_tag, 'pass_int')
 
+        '''removes last row because it's empty'''
+        playerPassYards = playerPassYards[0:len(playerPassYards) - 1]
 
-
+        '''sorts list by Team then Passing Yards'''
+        sortedList = sorted(zip(playerNames, playerTeams, playerAge, 
+                                [int(x) for x in playerPassYards], playerComp, 
+                                playerAtt, playerYardsPerPass, playerInt, playerTD ), key=lambda x: (x[1], -x[3]), reverse=False)
+        
+        '''prints to make sure the data is being correctly gathered'''
+        for names, teams, age, yards, comp, att, ypp, inter, td in sortedList:
+            print(f'{names}, {teams}, {age}, {yards}, {comp}, {att}, {ypp}, {inter}, {td}')
+        
+        return sortedList
